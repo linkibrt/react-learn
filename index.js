94b268da604f15207f30323e4d9cf23d73c90bf7
linkibrt <linkibrt@gmail.com>
2020-03-28 12:17:56 +0530

    Element Variables

--- index.js
+++ index.js
@@ -4,6 +4,41 @@ import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
 
+class LoginControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    this.state = {isLoggedIn: false};
+  }
+
+  handleLoginClick() {
+    this.setState({isLoggedIn: true});
+  }
+
+  handleLogoutClick() {
+    this.setState({isLoggedIn: false});
+  }
+
+  render() {
+    const isLoggedIn = this.state.isLoggedIn;
+    let button;
+
+    if (isLoggedIn) {
+      button = <LogoutButton onClick={this.handleLogoutClick} />;
+    } else {
+      button = <LoginButton onClick={this.handleLoginClick} />;
+    }
+
+    return (
+      <div>
+        <Greeting isLoggedIn={isLoggedIn} />
+        {button}
+      </div>
+    );
+  }
+}
+
 function UserGreeting(props) {
   return <h1>Welcome back!</h1>;
 }
@@ -11,6 +46,7 @@ function UserGreeting(props) {
 function GuestGreeting(props) {
   return <h1>Please sign up.</h1>;
 }
+
 function Greeting(props) {
   const isLoggedIn = props.isLoggedIn;
   if (isLoggedIn) {
@@ -19,8 +55,23 @@ function Greeting(props) {
   return <GuestGreeting />;
 }
 
+function LoginButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Login
+    </button>
+  );
+}
+
+function LogoutButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Logout
+    </button>
+  );
+}
+
 render(
-  // Try changing to isLoggedIn={true}:
-  <Greeting isLoggedIn={false} />,
+  <LoginControl />,
   document.getElementById('root')
 );
